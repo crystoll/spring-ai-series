0d34d575dad32a2aa535c06219d01d85750a545a
Arto Santala <crystoll@gmail.com>
2024-11-09 14:00:22 +0200

    ChatBot is now stateful by using InMemoryChatMemory implementation

--- src/main/java/com/soturit/springai/ChatBotController.java
+++ src/main/java/com/soturit/springai/ChatBotController.java
@@ -1,7 +1,8 @@
 package com.soturit.springai;
 
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.http.ResponseEntity;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,15 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/chatbot")
 public class ChatBotController {
-    private final ChatClient.Builder chatClientBuilder;
+    private final ChatClient chatClient;
 
     public ChatBotController(ChatClient.Builder chatClientBuilder) {
-        this.chatClientBuilder = chatClientBuilder;
+        this.chatClient = chatClientBuilder
+        .defaultAdvisors(new MessageChatMemoryAdvisor(new InMemoryChatMemory()))// CHAT MEMORY
+        .build();
     }
 
-    @GetMapping("/chatthatforgets")
-    public ChatBotResponse getChatThatForgetsResponse(@RequestParam String prompt) {
-        var chatClient = chatClientBuilder.build();
+    @GetMapping("/chat")
+    public ChatBotResponse getChatResponse(@RequestParam String prompt) {
         return new ChatBotResponse(chatClient.prompt(prompt).call().content());
     }
 
